897d64a948d5254e98a89eaa9e8f5c5bf66379c7
shivapatra17 <shivatesting17@gmail.com>
2021-06-21 15:31:43 +0530

    New Project created and submitting

---
+++ src/main/java/cucumberframework/PracticeProjectWithCucumber/App.java
@@ -0,0 +1,13 @@
+package cucumberframework.PracticeProjectWithCucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ActionsofParticularPage/HomePageActions.java
@@ -0,0 +1,5 @@
+package ActionsofParticularPage;
+
+public class HomePageActions {
+
+}

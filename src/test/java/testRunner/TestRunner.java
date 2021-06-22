77bd19afbe05cb8d9c9c5f1c2b31d5a264cabc7d
shivapatra17 <shivatesting17@gmail.com>
2021-06-22 11:58:11 +0530

    Modified TestRunner Class

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -2,7 +2,14 @@ package testRunner;
 
 import org.junit.runner.RunWith;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+ features = "Feature"
+ ,glue={"stepDefinition"}
+ )
 public class TestRunner {
 
 
